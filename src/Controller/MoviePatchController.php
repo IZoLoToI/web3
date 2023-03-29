76f36abeb248298232661958fb87a498acd8c8a1
IZoLoToI <104593248+IZoLoToI@users.noreply.github.com>
2023-03-29 13:38:28 +0300

    Update MoviePatchController.php

--- src/Controller/MoviePatchController.php
+++ src/Controller/MoviePatchController.php
@@ -4,43 +4,32 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Movie;
+use App\Entity\Potato;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MoviePatchController extends AbstractController
+class PotatoPatchController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
-
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
     }
-
-    /**
-     * @Route("/patchmovie/{id}", methods={"PATCH"})
-     */
     public function patch(Request $request, int $id): Response
     {
-        $book = $this->entityManager->getRepository(Movie::class)->find($id);
-
+        $book = $this->entityManager->getRepository(Potato::class)->find($id);
         if (!$book) {
-            throw $this->createNotFoundException('Нет такого фильма с id ' . $id);
+            throw $this->createNotFoundException('Potato not found with id ' . $id);
         }
-
         $data = json_decode($request->getContent(), true);
-
         if (isset($data['nameCinema'])) {
-            $book->setNameCinema($data['nameCinema']);
+            $book->setColor($data['color']);
         }
-
         if (isset($data['nameMovie'])) {
-            $book->setNameMovie($data['nameMovie']);
+            $book->setNameMovie($data['shape']);
         }
-
         $this->entityManager->flush();
-
-        return $this->json(['message' => 'Фильмы обновлены']);
+        return $this->json(['message' => 'Potatoes updated']);
     }
 }
