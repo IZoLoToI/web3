88164cfe989ed99bffca972817f0eeecdcdd3dcd
IZoLoToI <104593248+IZoLoToI@users.noreply.github.com>
2023-03-29 13:48:39 +0300

    Update PotatoController.php

--- src/Controller/PotatoController.php
+++ src/Controller/PotatoController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Shape;
+use App\Entity\Potato;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
