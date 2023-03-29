a7b06d762cde51c0e358794759ff1ed979989d27
IZoLoToI <104593248+IZoLoToI@users.noreply.github.com>
2023-03-29 14:08:19 +0300

    Update PotatoPutController.php

--- src/Controller/PotatoPutController.php
+++ src/Controller/PotatoPutController.php
@@ -4,42 +4,39 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Movie;
+use App\Entity\Potato;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MoviePutController extends AbstractController
+class PotatoPutController extends AbstractController
 {
-
     private EntityManagerInterface $entityManager;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
     }
-    /**
-     * @Route("/updatemovie/{id}", methods={"PUT"})
-     */
+
     public function put(Request $request, $id): Response
     {
-        $movie = $this->entityManager->getRepository(Movie::class)->find($id);
+        $potato = $this->entityManager->getRepository(Potato::class)->find($id);
 
-        if (!$movie) {
-            throw $this->createNotFoundException('Нет такого фильма с id ' . $id);
+        if (!$potato) {
+            throw $this->createNotFoundException('Potato not found with id ' . $id);
         }
 
         $data = json_decode($request->getContent(), true);
 
-        $nameCinema = isset($data['nameCinema']) ? $data['nameCinema'] : null;
-        $nameMovie = isset($data['nameMovie']) ? $data['nameMovie'] : null;
+        $color = isset($data['color']) ? $data['color'] : null;
+        $shape = isset($data['shape']) ? $data['shape'] : null;
 
-        $movie->setNameCinema($nameCinema);
-        $movie->setNameMovie($nameMovie);
+        $potato->setColor($color);
+        $potato->setShape($shape);
 
         $this->entityManager->flush();
 
-        return $this->json(['id' => $movie->getId()]);
+        return $this->json(['id' => $potato->getId()]);
     }
 
 }
